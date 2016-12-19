053c0cb1b5e09508287d98e15ce4b57c1650a264
dofine <dufandf@gmail.com>
2016-12-19 20:09:30 +0800

    Test qt5

--- TreeWidgetTest.py
+++ TreeWidgetTest.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 import sys
-from PyQt4 import QtGui,QtCore
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtWidgets import QTreeWidget, QLabel, QTreeWidgetItem, QWidget, QMainWindow, QApplication, QPushButton, QAbstractItemView
 
-# class Label(QtGui.QLabel):
+# class Label(QLabel):
 #     def __init__(self,flag):
 #         super(Label, self).__init__()
 #         self.setText(u"测试")
 #         self.setProperty("flag",flag) # 有Flag就变色
 #         self.setFixedHeight(40)
 
-class Label(QtGui.QWidget):
+class Label(QWidget):
     def __init__(self,flag):
         super(Label, self).__init__()
         self.font = QtGui.QFont()
@@ -23,12 +24,12 @@ class Label(QtGui.QWidget):
             self.setStyleSheet("background:rgb(37,70,79);")
         # self.resize(150,40)
 
-        self.label = QtGui.QLabel(self)
+        self.label = QLabel(self)
         self.label.setText(u"测试")
         self.label.move(50,5)
         self.label.setFont(self.font)
         # self.label.setStyleSheet("{color:r:}")
-        self.label2 = QtGui.QLabel(self)
+        self.label2 = QLabel(self)
         self.label2.setText(u"测试")
         self.label2.move(50,20)
         self.font.setPixelSize(10)
@@ -39,23 +40,23 @@ class Label(QtGui.QWidget):
         self.setProperty("flag",flag) # 有Flag就变色
         self.setFixedHeight(40)
 
-class TreeWidget(QtGui.QMainWindow):
+class TreeWidget(QMainWindow):
     def __init__(self,parent=None):
         super(TreeWidget,self).__init__()
 
-        self.tree = QtGui.QTreeWidget()
+        self.tree = QTreeWidget()
         for i in range(1,10):
-            QtGui.QTreeWidgetItem(self.tree) # 生成十行控件
+            QTreeWidgetItem(self.tree) # 生成十行控件
 
         self.tree.setItemWidget(self.tree.topLevelItem(0),0,Label(False))
         self.tree.setItemWidget(self.tree.topLevelItem(1),0,Label(True))
         self.tree.setItemWidget(self.tree.topLevelItem(2),0,Label(False))
         self.tree.setItemWidget(self.tree.topLevelItem(3),0,Label(True))
 
-        self.tree.setItemWidget(self.tree.topLevelItem(4),0,QtGui.QPushButton("Push ME"))
-        self.tree.setItemWidget(self.tree.topLevelItem(5),0,QtGui.QPushButton("Push ME"))
-        self.tree.setItemWidget(self.tree.topLevelItem(6),0,QtGui.QPushButton("Push ME"))
-        self.tree.setItemWidget(self.tree.topLevelItem(7),0,QtGui.QPushButton("Push ME"))
+        self.tree.setItemWidget(self.tree.topLevelItem(4),0,QPushButton("Push ME"))
+        self.tree.setItemWidget(self.tree.topLevelItem(5),0,QPushButton("Push ME"))
+        self.tree.setItemWidget(self.tree.topLevelItem(6),0,QPushButton("Push ME"))
+        self.tree.setItemWidget(self.tree.topLevelItem(7),0,QPushButton("Push ME"))
 
         self.tree.setHeaderHidden(True)     # 隐藏头部
         self.tree.setRootIsDecorated(False) # 去除根结点缩进,去掉虚线边框
@@ -72,7 +73,7 @@ class TreeWidget(QtGui.QMainWindow):
             }
             ''')
 
-        return
+        # return
         #设置列数
         #self.tree.setColumnCount(1)
         #设置头部
@@ -86,7 +87,7 @@ class TreeWidget(QtGui.QMainWindow):
         #列宽?两列以上起作用
         #self.tree.setColumnWidth(10,100)
         #单选
-        self.tree.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
+        self.tree.setSelectionMode(QAbstractItemView.SingleSelection)
         #可滑动多选
         #self.tree.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
         #self.tree.setSelection(QtCore.QRect(1,2,3,4),QtGui.QItemSelectionModel.Current)
@@ -100,7 +101,7 @@ class TreeWidget(QtGui.QMainWindow):
         #Plastique Style
         #self.tree.setStyle(QtGui.QStyleFactory.create("plastique"));
         #选中整行
-        self.tree.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+        self.tree.setSelectionBehavior(QAbstractItemView.SelectRows)
         #只选单个Item
         #self.tree.setSelectionBehavior(QtGui.QAbstractItemView.SelectItems)
         #缩进
@@ -112,7 +113,7 @@ class TreeWidget(QtGui.QMainWindow):
         #去除虚线
         self.tree.setFocusPolicy(QtCore.Qt.NoFocus)
 
-        root = QtGui.QTreeWidgetItem(self.tree)
+        root = QTreeWidgetItem(self.tree)
         root.setText(0,u'发现')
         #ICON
         #root.setIcon(0, QtGui.QIcon('header.png'))
@@ -121,54 +122,54 @@ class TreeWidget(QtGui.QMainWindow):
         #交替颜色
         #self.tree.setAlternatingRowColors(True)
 
-        child1 = QtGui.QTreeWidgetItem(root)
+        child1 = QTreeWidgetItem(root)
         child1.setText(0,u'今日推荐')
         child1.setIcon(0,QtGui.QIcon('img/tree/section_today_recommends.png'))
 
-        child2 = QtGui.QTreeWidgetItem(root)
+        child2 = QTreeWidgetItem(root)
         child2.setText(0,u'虾小米精选')
         child2.setIcon(0,QtGui.QIcon('img/tree/section_suggest_collects.png'))
 
-        child3 = QtGui.QTreeWidgetItem(root)
+        child3 = QTreeWidgetItem(root)
         child3.setText(0,u'音乐排行榜') 
         child3.setIcon(0,QtGui.QIcon('img/tree/section_top_songs.png'))
 
-        child4 = QtGui.QTreeWidgetItem(root)
+        child4 = QTreeWidgetItem(root)
         child4.setText(0,u'音乐电台')
         child4.setIcon(0,QtGui.QIcon('img/tree/section_radios.png'))
 
-        child5 = QtGui.QTreeWidgetItem(root)
+        child5 = QTreeWidgetItem(root)
         child5.setText(0,u'猜你喜欢')
         child5.setIcon(0,QtGui.QIcon('img/tree/section_guess_you_like.png'))
                         
-        root2 = QtGui.QTreeWidgetItem(self.tree)
+        root2 = QTreeWidgetItem(self.tree)
         root2.setText(0,u'我的音乐')
         self.tree.expandItem(root2)
 
-        child6 = QtGui.QTreeWidgetItem(root2)
+        child6 = QTreeWidgetItem(root2)
         child6.setText(0,u'本地音乐')
         child6.setIcon(0,QtGui.QIcon('img/tree/section_itunes.png'))
 
-        child7 = QtGui.QTreeWidgetItem(root2)
+        child7 = QTreeWidgetItem(root2)
         child7.setText(0,u'播放记录')
         child7.setIcon(0,QtGui.QIcon('img/tree/section_playlogs.png'))
 
-        child8 = QtGui.QTreeWidgetItem(root2)
+        child8 = QTreeWidgetItem(root2)
         child8.setText(0,u'联想歌单')
         child8.setIcon(0,QtGui.QIcon('img/tree/section_more_like.png'))
 
-        child9 = QtGui.QTreeWidgetItem(root2)
+        child9 = QTreeWidgetItem(root2)
         child9.setText(0,u'我的收藏')
         child9.setIcon(0,QtGui.QIcon('img/tree/section_favorites.png'))
 
-        child10 = QtGui.QTreeWidgetItem(root2)
+        child10 = QTreeWidgetItem(root2)
         child10.setText(0,u'精选集')
         child10.setIcon(0,QtGui.QIcon('img/tree/section_collects.png'))
 
-        child11 = QtGui.QTreeWidgetItem(root2)
+        child11 = QTreeWidgetItem(root2)
         child11.setText(0,u'离线音乐')
         child11.setIcon(0,QtGui.QIcon('img/tree/section_offline_music.png'))
-        child11.setProperty("ter","test")
+        # child11.setProperty("ter","test")
 
         self.setCentralWidget(self.tree)
         self.resize(200,450)
@@ -185,19 +186,19 @@ class TreeWidget(QtGui.QMainWindow):
         }
             ''')
 
-class MyTree(QtGui.QTreeWidget):
+class MyTree(QTreeWidget):
     """docstring for MyTree"""
     def __init__(self, parent=None):
         super(MyTree, self).__init__()
 
     def drawRow(self,painter,option,index):
         painter.save()
-        brush = QtGui.QBrush(QtGui.QColor(255,240,180))
+        brush = QBrush(QtGui.QColor(255,240,180))
         painter.fillRect(option.rect,brush)
         painter.restore()
-        QtGui.QTreeWidget.drawRow(self,painter,option,index)
+        QTreeWidget.drawRow(self,painter,option,index)
 
-app = QtGui.QApplication(sys.argv)
+app = QApplication(sys.argv)
 testWidget = TreeWidget()
 testWidget.show()
 app.exec_()
